36a32242bbdafa495ed2bd594027944c9649ff15
BIELBY <Richard.BIELBY@education.gov.uk>
2022-03-22 17:54:22 +0000

    Final run of tidy_code now that shinytest is passing in Github actions.

--- tests/shinytest/initial_load_test.R
+++ tests/shinytest/initial_load_test.R
@@ -1,5 +1,7 @@
 app <- ShinyDriver$new("../../")
 app$snapshotInit("initial_load_test", screenshot = FALSE)
 
-app$snapshot(items=list(input=c('bins'),
-                        output=c('box_info')))
+app$snapshot(items = list(
+  input = c("bins"),
+  output = c("box_info")
+))
