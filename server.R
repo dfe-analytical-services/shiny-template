511f7b03ed82c96f9110d9618a28e7fdfb582de0
Rich Bielby <richard.bielby@education.gov.uk>
2022-08-24 16:46:20 +0100

    Ran tidy_code.

--- server.R
+++ server.R
@@ -37,8 +37,8 @@ server <- function(input, output, session) {
 
   # Define server logic required to draw a histogram
   output$lineRevBal <- renderPlotly({
-    ggplotly(createAvgRevTimeSeries(reactiveRevBal(), input$selectArea)) %>% 
-      config(displayModeBar = F)%>%
+    ggplotly(createAvgRevTimeSeries(reactiveRevBal(), input$selectArea)) %>%
+      config(displayModeBar = F) %>%
       layout(legend = list(orientation = "h", x = 0, y = -0.2))
   })
 
@@ -52,9 +52,9 @@ server <- function(input, output, session) {
   })
 
   output$colBenchmark <- renderPlotly({
-    ggplotly(plotAvgRevBenchmark(reactiveBenchmark()) %>% 
-               config(displayModeBar = F),
-      height = 420
+    ggplotly(plotAvgRevBenchmark(reactiveBenchmark()) %>%
+      config(displayModeBar = F),
+    height = 420
     )
   })
 
