afca22c64224addb041605b30614dd820d4f6639
Rich Bielby <richard.bielby@education.gov.uk>
2022-08-24 15:18:09 +0100

    Added IDs to mirror site links.

--- ui.R
+++ ui.R
@@ -79,8 +79,8 @@ ui <- function(input, output, session) {
       paste0(
         "This Dashboard is in beta phase and we are still reviewing performance and reliability. ",
         "In case of slowdown or connection issues due to high demand, we have produced two instances of this site which can be accessed at the following links: ",
-        "<a href=", site_primary, ", id='site_1'>Site 1</a> and ",
-        "<a href=", site_overflow, ", id='site_2'>Site 2</a>."
+        "<a href=", site_primary, " id='link_site_1'>Site 1</a> and ",
+        "<a href=", site_overflow, " id='link_site_2'>Site 2</a>."
       )
     ),
     shiny::navlistPanel(
