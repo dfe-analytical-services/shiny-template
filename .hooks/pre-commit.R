a1eba9143f93022851c68d4948f51035153bb2a4
Rich Bielby <richard.bielby@education.gov.uk>
2022-09-06 14:39:00 +0100

    Testing hook.

--- .hooks/pre-commit.R
+++ .hooks/pre-commit.R
@@ -40,7 +40,7 @@ for (file in current_files$files) {
 }
 
 if(grepl('G-Z967JJVQQX', htmltools::includeHTML(("google-analytics.html"))) & 
-   !(toupper(Sys.getenv("USERNAME")) %in% c("CFOSTER4", "CRACE", "LSELBY", "RBIELBY"))){
+   !(toupper(Sys.getenv("USERNAME")) %in% c("CFOSTER4", "CRACE", "LSELBY"))){
   cat("Cleaning out the template's Google Analytics tag.",fill=TRUE)
   gsub_file("google-analytics.html", pattern = "G-Z967JJVQQX", replacement = "G-XXXXXXXXXX")
   system2(command = "git", args=c("add","google-analytics.html"))
