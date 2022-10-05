a41d0228399f71dd39aee697018da84e0260c857
Rich Bielby <richard.bielby@education.gov.uk>
2022-10-05 11:33:41 +0100

    Updated shiny tests

--- tests/shinytest/initial_load_test.R
+++ tests/shinytest/initial_load_test.R
@@ -1,7 +1,4 @@
-app <- ShinyDriver$new("../../", loadTimeout = 6.e4)
+app <- ShinyDriver$new("../../", loadTimeout = 6.4e4)
 app$snapshotInit("initial_load_test", screenshot = FALSE)
 
-app$snapshot(items = list(
-  input = c("bins"),
-  output = c("box_info")
-))
+app$snapshot()
