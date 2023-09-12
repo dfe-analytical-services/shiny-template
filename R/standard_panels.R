8ffb9a9b54b673aebe35f6f25ce035827ff390d6
MACHIN <Jen.MACHIN@EDUCATION.GOV.UK>
2023-09-12 11:24:19 +0100

    tidy_code has been run to try and stop the error from before

--- R/standard_panels.R
+++ R/standard_panels.R
@@ -47,24 +47,34 @@ support_links <- function() {
           width = 12,
           h1("Support and feedback"),
           h2("Give us feedback"),
-          p("This dashboard is a new service that we are developing. If you have any feedback or suggestions for improvements, please submit them using our ",
-          a(
-            href = "https://forms.office.com",
-            "feedback form", .noWS = c("after")
-          ), "."),
-          p("If you spot any errors or bugs while using this dashboard, please screenshot and email them to ",
-          a(href = "mailto:statistics.development@education.gov.uk", "statistics.development@education.gov.uk", .noWS = c("after")), "."),
+          p(
+            "This dashboard is a new service that we are developing. If you have any feedback or suggestions for improvements, please submit them using our ",
+            a(
+              href = "https://forms.office.com",
+              "feedback form", .noWS = c("after")
+            ), "."
+          ),
+          p(
+            "If you spot any errors or bugs while using this dashboard, please screenshot and email them to ",
+            a(href = "mailto:statistics.development@education.gov.uk", "statistics.development@education.gov.uk", .noWS = c("after")), "."
+          ),
           h2("Find more information on the data"),
-          p("The data used to produce the dashboard, along with methodological information can be found on ",
-          a(href = "https://explore-education-statistics.service.gov.uk/", "Explore Education Statistics", .noWS = c("after")),
-          "."),
+          p(
+            "The data used to produce the dashboard, along with methodological information can be found on ",
+            a(href = "https://explore-education-statistics.service.gov.uk/", "Explore Education Statistics", .noWS = c("after")),
+            "."
+          ),
           h2("Contact us"),
-          p("If you have questions about the dashboard or data within it, please contact us at ",
-          a(href = "mailto:statistics.development@education.gov.uk", "statistics.development@education.gov.uk", .noWS = c("after"))),
+          p(
+            "If you have questions about the dashboard or data within it, please contact us at ",
+            a(href = "mailto:statistics.development@education.gov.uk", "statistics.development@education.gov.uk", .noWS = c("after"))
+          ),
           h2("See the source code"),
-          p("The source code for this dashboard is available in our ",
-          a(href = "https://github.com/dfe-analytical-services/shiny-template", "GitHub repository", .noWS = c("after")),
-          ".")
+          p(
+            "The source code for this dashboard is available in our ",
+            a(href = "https://github.com/dfe-analytical-services/shiny-template", "GitHub repository", .noWS = c("after")),
+            "."
+          )
         ),
         column(
           12,
