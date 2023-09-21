77e414418c6377dc4c367e63d88a5d36c030c060
MACHIN <Jen.MACHIN@EDUCATION.GOV.UK>
2023-09-21 12:59:30 +0100

    forgot to run tidy_code before committing the first time

--- R/dfe_resources.R
+++ R/dfe_resources.R
@@ -5,6 +5,6 @@
 # (the two commented out hex codes are for the suggested colours for two
 # additional series if absolutely necessary).
 # 2023-09-21: update made to change hex codes to AF colours package
-# guidance here: https://best-practice-and-impact.github.io/afcolours/ 
+# guidance here: https://best-practice-and-impact.github.io/afcolours/
 
 gss_colour_pallette <- afcolours::af_colours("categorical", colour_format = "hex", n = 6)
