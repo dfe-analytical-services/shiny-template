12cdf4b504aa9365f17b961a8f254682652a785d
Cam Race <cameron.race@education.gov.uk>
2024-07-20 13:53:24 +0100

    remove expandable section and lots of CSS

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,1 @@
 # Add custom functions used across multiple places in the app into this script
-
-expandable <- function(input_id, label, contents) {
-  gov_details <- shiny::tags$details(
-    class = "govuk-details", id = input_id,
-    shiny::tags$summary(
-      class = "govuk-details__summary",
-      shiny::tags$span(
-        class = "govuk-details__summary-text",
-        label
-      )
-    ),
-    shiny::tags$div(contents)
-  )
-}
