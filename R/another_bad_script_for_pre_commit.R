836ae7863ce993b6cafc4aef5c733c603c96e1f8
alex-rogers-hub <alex@rogersanalytics.co.uk>
2025-02-13 16:42:34 +0000

    commit allowed?

---
+++ R/another_bad_script_for_pre_commit.R
@@ -0,0 +1,7 @@
+bad_style_func <- function(){'bad thing here really really really really really really really really really really really really really long line too'}
+
+badFormatOfVar <- 'not snake format'
+
+weird_indent <- data.frame("a" = 1,
+                              "b" = 2,
+                    "c" = 3)
