eb7041659f7d7c68e357e8b575acbde6e30b8a4a
alex-rogers-hub <alex@rogersanalytics.co.uk>
2025-02-14 12:33:05 +0000

    remove unused function

--- R/plotting.R
+++ R/plotting.R
@@ -8,47 +8,6 @@
 # scripts that contain your custom plotting functions.
 # -----------------------------------------------------------------------------
 
-# Revenue balance time series line chart --------------------------------------
-create_avg_rev_timeseries <- function(df, input_area) {
-  ggplot(df, aes(
-    x = year,
-    y = average_revenue_balance,
-    color = area_name,
-    id = area_name
-  )) +
-    geom_line_interactive(size = 1) +
-    geom_point_interactive(
-      aes(
-        tooltip = paste0(
-          "<p><b>", area_name, ", ", year, "</b></p>",
-          unlist(lapply(average_revenue_balance, pretty_num, prefix = gbp)), "</p>"
-        )
-      ),
-      size = 0.5
-    ) +
-    theme_classic() +
-    theme(
-      text = element_text(size = 12),
-      axis.title.x = element_text(margin = margin(t = 12)),
-      axis.title.y = element_text(
-        angle = 0, vjust = 0.5,
-        margin = margin(r = 12)
-      ),
-      axis.line = element_line(linewidth = 0.75),
-      legend.position = "top"
-    ) +
-    scale_y_continuous(
-      labels = scales::number_format(accuracy = 1, big = ",", prefix = gbp)
-    ) +
-    xlab("Academic year end") +
-    ylab(str_wrap("Average revenue balance", 16)) +
-    scale_color_manual(
-      "Area",
-      breaks = unique(c("England", input_area)),
-      values = gss_colour_pallette
-    )
-}
-
 # Revenue balance bar chart ---------------------------------------------------
 plot_avg_rev_benchmark <- function(df_revenue_balance, input_area) {
   ggplot(df_revenue_balance, aes(
