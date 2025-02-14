39b14592d8011a06ed642a85ecb3aca7caaa8666
Olu Adetayo <adetayoseun@yahoo.com>
2025-02-14 09:28:54 +0000

    fixing lintr issues from a different pr

--- server.R
+++ server.R
@@ -118,7 +118,6 @@ server <- function(input, output, session) {
   reactive_map_to_display <- reactive({
     leaflet(reactive_map_dataset()) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
-      # addTiles() %>%
       setView(lng = -3.95, lat = 53, zoom = 5.5) %>%
       addPolygons(
         color = "black",
