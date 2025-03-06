0bf6640fb3016a24c50a9177cc0675364003a6ad
alex-rogers-hub <alex@rogersanalytics.co.uk>
2025-03-06 09:53:31 +0000

    styler

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -92,13 +92,13 @@ create_output_tabs <- function(
 #' This function generates a link to an internal tabPanel (target_link),
 #' with the link text specified in "link_text"
 #' The following is required in the server.R script
-#' 
+#'
 #'   # navigation link within text --------------------------------------------
 #' observeEvent(input$nav_link, {
 #'   shiny::updateTabsetPanel(session, "navlistPanel", selected = input$nav_link)
 #' })
-#' 
-#' The target location could be changed to a different UI element by 
+#'
+#' The target location could be changed to a different UI element by
 #' changing the "navlistPanel" element of the server code
 
 in_line_nav_link <- function(link_text, target_link) {
