fa7bdb9ea675df246e04b1804e467e1a7a141155
alex-rogers-hub <alex@rogersanalytics.co.uk>
2025-03-14 13:00:13 +0000

    Link in text (#144)
    * family sans... but will rebase, didn't mean to do it on main
    
    * initial commit
    
    * adding in line nav link, noti_banner, warning_text and external_link
    
    * function documentation in helper file
    
    * styler
    
    * tests for in_line_nav_link function
    
    ---------
    
    Co-authored-by: Rich Bielby <richard.bielby@education.gov.uk>

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -86,3 +86,27 @@ create_output_tabs <- function(
 
   do.call(tabsetPanel, c(list(id = paste0("main_tabs_", id)), tabs))
 }
+
+#' Standardise internal links ---------------------------------------------
+#'
+#' This function generates a link to an internal tabPanel (target_link),
+#' with the link text specified in "link_text"
+#' The following is required in the server.R script
+#'
+#'   # navigation link within text --------------------------------------------
+#' observeEvent(input$nav_link, {
+#'   shiny::updateTabsetPanel(session, "navlistPanel", selected = input$nav_link)
+#' })
+#'
+#' The target location could be changed to a different UI element by
+#' changing the "navlistPanel" element of the server code
+
+in_line_nav_link <- function(link_text, target_link) {
+  HTML(paste0(
+    "<a href='#' onclick=\"Shiny.setInputValue('nav_link', '",
+    target_link,
+    "', {priority: 'event'});\">",
+    link_text,
+    "</a>"
+  ))
+}
