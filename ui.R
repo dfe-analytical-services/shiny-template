01c7e8b216fdf2841e16d351d01e36e222bd7ec0
Olu Adetayo <adetayoseun@yahoo.com>
2025-03-17 09:01:04 +0000

    hide left sidebar and switch to links

--- ui.R
+++ ui.R
@@ -24,7 +24,7 @@ ui <- function(input, output, session) {
     tags$head(HTML("<title>Department for Education (DfE) Shiny Template</title>")),
     tags$head(tags$link(rel = "shortcut icon", href = "dfefavicon.png")),
     use_shiny_title(),
-    useShinyjs(),
+    # useShinyjs(),
     tags$html(lang = "en"),
     # Add meta description for search engines
     meta() %>%
@@ -82,55 +82,70 @@ ui <- function(input, output, session) {
       "Beta",
       "This dashboard is in beta phase and we are still reviewing performance and reliability."
     ),
-
-    # Nav panels --------------------------------------------------------------
-    shiny::navlistPanel(
-      "",
-      id = "navlistPanel",
-      widths = c(2, 8),
-      well = FALSE,
-      # Content for these panels is defined in the R/ui_panels/ folder
-      example_tab_1_panel(),
-      user_guide_panel(),
-      shiny::tabPanel(
-        value = "a11y_panel",
-        "Accessibility",
-        dfeshiny::a11y_panel(
-          dashboard_title = site_title,
-          dashboard_url = site_primary,
-          date_tested = "12th March 2024",
-          date_prepared = "1st July 2024",
-          date_reviewed = "1st July 2024",
-          issues_contact = "explore.statistics@education.gov.uk",
-          non_accessible_components = c("List non-accessible components here"),
-          specific_issues = c("List specific issues here")
+    gov_main_layout(
+      bslib::navset_hidden(
+        id = "navlistPanel",
+        nav_panel(
+          "dashboard",
+          ## Main dashboard ---------------------------------------------------
+          layout_columns(
+            # Override default wrapping breakpoints to avoid text overlap
+            col_widths = breakpoints(sm = c(4, 8), md = c(3, 9), lg = c(2, 9)),
+            ## Left navigation ------------------------------------------------
+            dfe_contents_links(
+              links_list =
+                c(
+                  "Example tab 1",
+                  "User guide",
+                  "Accessibility",
+                  "Cookies",
+                  "Support and feedback"
+                )
+            ),
+            ## Dashboard panels -----------------------------------------------
+            bslib::navset_hidden(
+              id = "left_nav",
+              example_tab_1_panel(),
+              user_guide_panel(),
+              bslib::nav_panel(
+                "a11y_panel",
+                dfeshiny::a11y_panel(
+                  dashboard_title = site_title,
+                  dashboard_url = site_primary,
+                  date_tested = "12th March 2024",
+                  date_prepared = "1st July 2024",
+                  date_reviewed = "1st July 2024",
+                  issues_contact = "explore.statistics@education.gov.uk",
+                  non_accessible_components = c("List non-accessible components here"),
+                  specific_issues = c("List specific issues here")
+                )
+              ),
+              bslib::nav_panel(
+                "cookies_panel_ui",
+                cookies_panel_ui(google_analytics_key = google_analytics_key)
+              ),
+              bslib::nav_panel(
+                "support_panel_ui",
+                support_panel(
+                  team_email = "explore.statistics@education.gov.uk",
+                  repo_name = "https://github.com/dfe-analytical-services/shiny-template",
+                  form_url = "https://forms.office.com"
+                )
+              )
+            )
+          )
+        ),
+        ## Footer pages -------------------------------------------------------
+        shinyGovstyle::footer(
+          full = TRUE,
+          links = c(
+            "Accessibility statement",
+            "Use of cookies",
+            "Support and feedback",
+            "Privacy notice",
+            "External link"
+          )
         )
-      ),
-      shiny::tabPanel(
-        value = "cookies_panel_ui",
-        "Cookies",
-        cookies_panel_ui(google_analytics_key = google_analytics_key)
-      ),
-      shiny::tabPanel(
-        value = "support_panel_ui",
-        "Support and feedback",
-        support_panel(
-          team_email = "explore.statistics@education.gov.uk",
-          repo_name = "https://github.com/dfe-analytical-services/shiny-template",
-          form_url = "https://forms.office.com"
-        )
-      )
-    ),
-
-    # Footer ------------------------------------------------------------------
-    shinyGovstyle::footer(
-      full = TRUE,
-      links = c(
-        "Accessibility statement",
-        "Use of cookies",
-        "Support and feedback",
-        "Privacy notice",
-        "External link"
       )
     )
   )
