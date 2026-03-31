4ce80f6b00683264d8a4c3e47e1d42c6ea88b716
Cam Race <52536248+cjrace@users.noreply.github.com>
2026-03-31 14:26:32 +0100

    remove tidycode workflow and styler precommit hook

--- .hooks/pre-commit.R
+++ .hooks/pre-commit.R
@@ -4,7 +4,6 @@ shhh <- suppressPackageStartupMessages # It's a library, so shhh!
 shhh(library(dplyr))
 shhh(library(xfun))
 shhh(library(dfeshiny))
-shhh(library(styler))
 
 message("\n")
 
@@ -22,12 +21,16 @@ print(ign_files)
 
 # Run a pass through the .gitignore files and look for any issues
 if (ncol(ign_files) > 1) {
-  message("ERROR: It looks like you've got commas in the .gitignore. Please correct the .gitignore file and try again.")
+  message(
+    "ERROR: It looks like you've got commas in the .gitignore. Please correct the .gitignore file and try again."
+  )
   error_flag <- TRUE
 } else {
   for (i in 1:nrow(ign_files)) {
     if (grepl(" ", ign_files$filename[i])) {
-      message("ERROR: It looks like you've got spaces in filenames in the .gitignore. Please rename your files if they contain spaces and update the .gitignore file accordingly.")
+      message(
+        "ERROR: It looks like you've got spaces in filenames in the .gitignore. Please rename your files if they contain spaces and update the .gitignore file accordingly."
+      )
       error_flag <- TRUE
     }
   }
@@ -36,23 +39,42 @@ if (ncol(ign_files) > 1) {
 suffixes <- "xlsx$|ods$|dat$|csv$|tex$|pdf$|zip$|gz$|parquet$|rda$|rds$"
 
 current_files <- data.frame(files = list.files("./", recursive = TRUE)) %>%
-  filter(grepl(suffixes, files, ignore.case = TRUE), !grepl("renv|datafiles_log.csv", files))
+  filter(
+    grepl(suffixes, files, ignore.case = TRUE),
+    !grepl("renv|datafiles_log.csv", files)
+  )
 
 for (file in current_files$files) {
   if (!file %in% log_files$filename) {
     message("Error: ", file, " is not recorded in datafiles_log.csv.\n\n")
-    message("Please add an entry to datafiles_log.csv for this file and mark it as unpublished, published or reference.\n\n")
+    message(
+      "Please add an entry to datafiles_log.csv for this file and mark it as unpublished, published or reference.\n\n"
+    )
     error_flag <- TRUE
   } else {
     file_status <- (log_files %>% filter(filename == file))$status
-    if (!file_status %in% c("published", "Published", "reference", "Reference", "dummy", "Dummy")) {
+    if (
+      !file_status %in%
+        c("published", "Published", "reference", "Reference", "dummy", "Dummy")
+    ) {
       if (!file %in% ign_files$filename & !grepl("unpublished", file)) {
-        message("Error: ", file, " is not logged as published or reference data in datafiles_log.csv and is not found in .gitignore.\n\n")
-        message("If the file contains published or reference data then update its entry in datafiles_log.csv.\n\n")
-        message("If the file contains unpublished data then add it to the .gitignore file.\n\n")
+        message(
+          "Error: ",
+          file,
+          " is not logged as published or reference data in datafiles_log.csv and is not found in .gitignore.\n\n"
+        )
+        message(
+          "If the file contains published or reference data then update its entry in datafiles_log.csv.\n\n"
+        )
+        message(
+          "If the file contains unpublished data then add it to the .gitignore file.\n\n"
+        )
         error_flag <- TRUE
       } else {
-        message(file, " is recorded in the logfile as unpublished data and in .gitignore and so will not be included as part of the commit.\n\n")
+        message(
+          file,
+          " is recorded in the logfile as unpublished data and in .gitignore and so will not be included as part of the commit.\n\n"
+        )
       }
     }
   }
@@ -60,7 +82,9 @@ for (file in current_files$files) {
 
 
 if (error_flag) {
-  message("Warning, aborting commit. Unrecognised data files found, please update .gitignore or datafiles_log.csv.\n")
+  message(
+    "Warning, aborting commit. Unrecognised data files found, please update .gitignore or datafiles_log.csv.\n"
+  )
   quit(save = "no", status = 1, runLast = FALSE)
 } else {
   message("...all good!")
@@ -70,10 +94,17 @@ message("\n")
 
 message("2. Checking Google Analytics tag...\n")
 
-if (grepl("G-Z967JJVQQX", htmltools::includeHTML(("google-analytics.html"))) &
-  !(toupper(Sys.getenv("USERNAME")) %in% c("CFOSTER4", "CRACE", "LSELBY", "RBIELBY", "JMACHIN"))) {
+if (
+  grepl("G-Z967JJVQQX", htmltools::includeHTML(("google-analytics.html"))) &
+    !(toupper(Sys.getenv("USERNAME")) %in%
+      c("CFOSTER4", "CRACE", "LSELBY", "RBIELBY", "JMACHIN"))
+) {
   message("...cleaning out the template's Google Analytics tag.")
-  gsub_file("google-analytics.html", pattern = "G-Z967JJVQQX", replacement = "G-XXXXXXXXXX")
+  gsub_file(
+    "google-analytics.html",
+    pattern = "G-Z967JJVQQX",
+    replacement = "G-XXXXXXXXXX"
+  )
   gsub_file("ui.R", pattern = "Z967JJVQQX", replacement = "XXXXXXXXXX")
   system2(command = "git", args = c("add", "google-analytics.html"))
 } else {
@@ -81,33 +112,5 @@ if (grepl("G-Z967JJVQQX", htmltools::includeHTML(("google-analytics.html"))) &
 }
 
 message("\n")
-
-message("3. Checking code styling...\n")
-
-# Get list of staged R files
-staged_files <- system("git diff --cached --name-only --diff-filter=ACM | grep '\\.R$'", intern = TRUE)
-files_to_unstage <- c()
-
-message("Checking if staged R files are properly tidied...\n")
-for (file in staged_files) {
-  original <- readLines(file, warn = FALSE)
-  styled <- styler::style_text(original)
-
-  if (!identical(original, styled)) {
-    message(paste0("File ", file, " is not properly tidied. Removing from commit."))
-    files_to_unstage <- c(files_to_unstage, file)
-  }
-}
-
-if (length(files_to_unstage) > 0) {
-  system(paste0("git reset HEAD ", files_to_unstage, collapse = " "))
-  message(paste0(as.character(files_to_unstage), " was removed from the commit because it was not tidied.\n"))
-  message('Please format them using: Rscript -e \"styler::style_file(\\\"your_file.R\\\")\"')
-}
-
-if (length(staged_files) == 0) {
-  message("No R files staged for commit.\n")
-  quit(status = 0)
-}
 
 # End of hooks
